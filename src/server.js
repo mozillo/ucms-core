5a359747ea54ce13f87b14d7a3d8263764f45229
DengYun <tdzl2003@gmail.com>
2015-12-19 00:50:00 +0800

    change applyAction to doAction

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@
  * Created by tdzl2_000 on 2015-12-18.
  */
 
-import { applyAction } from './filter';
+import { doAction } from './filter';
 import Koa from 'koa';
 
 
 export async function startServer(options) {
-  await applyAction('init');
+  await doAction('init');
   const app = new Koa();
 
   app.listen(options.port, options.host);
